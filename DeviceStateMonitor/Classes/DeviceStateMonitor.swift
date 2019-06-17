ffb6cf99757281945fe40ac3911849bb3c4e5e65
Artur Kvaratshelia <artur.kvaratshelia@itomy.ch>
2019-06-17 14:16:40 +0300

    DDOS-72: Added availability attributes.

--- DeviceStateMonitor/Classes/DeviceStateMonitor.swift
+++ DeviceStateMonitor/Classes/DeviceStateMonitor.swift
@@ -20,6 +20,7 @@ import Foundation
 }
 
 /// Class containing description of thermal state device
+@available(iOS 11.0, *)
 public class ThermalState: ServiceState {
     public var thermalState: ProcessInfo.ThermalState
     public var service: DeviceService { return .thermal }
@@ -30,6 +31,7 @@ public class ThermalState: ServiceState {
 }
 
 /// Class containing description of battery state device
+@available(iOS 3.0, *)
 public class BatteryState: ServiceState {
     public var batteryState: UIDevice.BatteryState
     public var service: DeviceService { return .battery }
@@ -40,6 +42,7 @@ public class BatteryState: ServiceState {
 }
 
 /// Class containing description of power state device
+@available(iOS 9.0, *)
 public class PowerState: ServiceState {
     public var isLowPowerModeEnabled: Bool
     public var service: DeviceService { return .power }
